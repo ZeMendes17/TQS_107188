3c6411009e11ec040fd546b628605e4f8e8a0a89
Zé Mendes <mendes.j@ua.pt>
2024-02-29 17:56:03 +0000

    lab3_1 done. Added lab3_2

---
+++ lab3/lab3_2carService/src/main/java/pt/ua/deti/lab3_2carService/Lab32carServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.lab3_2carService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab32carServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab32carServiceApplication.class, args);
+	}
+
+}
---
+++ lab3/lab3_2carService/src/main/java/pt/ua/deti/lab3_2carService/entity/Car.java
@@ -0,0 +1,9 @@
+package pt.ua.deti.lab3_2carService;
+
+public class Car {
+    private Long carId;
+    private String maker;
+    private String model;
+
+    public Car() {}
+}
